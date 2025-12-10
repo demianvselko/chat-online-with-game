c8c541a83dadc3712f06616ee9f9fa3119eb70ba
demianvselko <vselkod@gmail.com>
2025-12-10 02:17:49 -0300

    chore(backend): rename infra folder

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,33 +1,33 @@
-import type { Config } from 'jest';
+import type { Config } from '@jest/types';
 
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: '.',
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.(spec|test)\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
-    '^@infra/(.*)$': '<rootDir>/src/infraestructure/$1',
-    '^@http/(.*)$': '<rootDir>/src/infraestructure/http/$1',
-    '^@mocks/(.*)$': '<rootDir>/test/__mocks__/$1',
-  },
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageReporters: ['text', 'lcov'],
-  coverageDirectory: 'coverage',
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    rootDir: '.',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    testRegex: String.raw`.*\.(spec|test)\.ts$`,
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
     },
-  },
+    moduleNameMapper: {
+        '^@src/(.*)$': '<rootDir>/src/$1',
+        '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+        '^@infra/(.*)$': '<rootDir>/src/infrastructure/$1',
+        '^@http/(.*)$': '<rootDir>/src/infrastructure/http/$1',
+        '^@mocks/(.*)$': '<rootDir>/test/__mocks__/$1',
+    },
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageReporters: ['text', 'lcov'],
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            statements: 80,
+            branches: 80,
+            functions: 80,
+            lines: 80,
+        },
+    },
 };
 
 export default config;
