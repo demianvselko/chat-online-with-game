0d4f02e6e1d20494ee53b99d85d6b587eecb65bd
Demián Vselko <54943405+demianvselko@users.noreply.github.com>
2025-12-10 02:30:53 -0300

    Chore/backend/rename infraestructure folder (#25)
    * Develop (#24)
    
    * chore(root): add foilder shared (#22)
    
    * Chore/root/add shared folder (#23)
    
    * chore(root): add foilder shared
    
    * feat(shared): add shared contracts and domain
    
    * feat(root): update package to add shrared in lint
    
    * feat(root): update package to add shrared in lint
    
    * fix(shared): lint config
    
    * chore(backend): rename infra folder
    
    * chore(backend): change prettier config
    
    * chore(root): update pnpm-lock
    
    * chore(root): update pnpm-lock v2

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from 'jest';
+import type { Config } from '@jest/types';
 
-const config: Config = {
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
   moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.(spec|test)\\.ts$',
+  testRegex: String.raw`.*\.(spec|test)\.ts$`,
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
     '^@domain/(.*)$': '<rootDir>/src/domain/$1',
-    '^@infra/(.*)$': '<rootDir>/src/infraestructure/$1',
-    '^@http/(.*)$': '<rootDir>/src/infraestructure/http/$1',
+    '^@infra/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@http/(.*)$': '<rootDir>/src/infrastructure/http/$1',
     '^@mocks/(.*)$': '<rootDir>/test/__mocks__/$1',
   },
   collectCoverage: true,
